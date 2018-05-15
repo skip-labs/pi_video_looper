ffc5c0c2bf159a33f32171a1158734ce9515aaca
Jørg Levermann <skip-labs@users.noreply.github.com>
2018-05-15 22:00:52 +0200

    Update video_looper.py
    update _animate_countdown

--- Adafruit_Video_Looper/video_looper.py
+++ Adafruit_Video_Looper/video_looper.py
@@ -152,8 +152,8 @@ class VideoLooper(object):
         message if the on screen display is enabled.
         """
         # Print message to console with number of movies in playlist.
-        message = '{0} Datei{1}.'.format(playlist.length(), 
-            'n' if playlist.length() >= 2 else '').' gefunden.'
+        message = '{0} Datei{1} gefunden.'.format(playlist.length(), 
+            'n' if playlist.length() >= 2 else '')
         self._print(message)
         # Do nothing else if the OSD is turned off.
         if not self._osd:
