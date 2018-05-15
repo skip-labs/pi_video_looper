cc97147281377ca33dddf0be535f306093456e27
Jørg Levermann <skip-labs@users.noreply.github.com>
2018-05-15 19:54:57 +0200

    Update usb_drive.py
    update idle_message with german translation

--- Adafruit_Video_Looper/usb_drive.py
+++ Adafruit_Video_Looper/usb_drive.py
@@ -38,7 +38,7 @@ class USBDriveReader(object):
 
     def idle_message(self):
         """Return a message to display when idle and no files are found."""
-        return 'Insert USB drive with compatible movies.'
+        return 'USB-Stick mit Videodateien (.mp4/h264) einstecken!'
 
 
 def create_file_reader(config):
